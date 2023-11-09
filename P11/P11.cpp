d0f0aad3587e9d72b374c424dc294a232a8948ff
RAY OTTO <rjotto123@gmail.com>
2023-11-09 13:04:44 -0500

    Display finished

--- P11/P11.cpp
+++ P11/P11.cpp
@@ -1,47 +1,55 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
+
 const int ROWS = 13;
 int findRowIndex(int thisRow);
 int findSeatIndex(char thisSeat);
 void displaySeatColumn(char seats[][ROWS], int column);
 
 int main(void) {
+    char seats[4][ROWS] = { {'D','D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D'}, {'C' ,'C' ,'C' ,'C' ,'C' ,'C' ,'C' ,'C' ,'C' ,'C' ,'C' ,'C'},{'B','B','B','B','B','B','B','B','B','B','B','B'},{'A','A','A','A','A','A','A','A','A','A','A','A'} };
+    
     while (true) {
-        cout << displaySeatColumn;
-        cout << "Enter a seat or Q to quit: ";
-        string input;
-        cin >> input;
-        if (input == "Q" || input == "q") {
-            break;
+        int i = 0;
+        int j = 0;
+
+        for (i = 0; i < 12; i++) {
+            cout << seats[-1][ROWS]<< ' ';
         }
-    }
-
-}
-
-int findRowIndex(int thisRow) { return 1; }
-
-int findSeatIndex(char thisSeat) { return 1; }
-
-void displaySeatColumn(char seats[][ROWS], int column)
-{
-    for (int row = 1; row <= 12; row++) {
-        cout << "D ";
-
-        if (row == 12) {
-            for (row <= 24; row++;) {
-                cout << "C ";
+        cout << endl;
+        if (i == 12) {
+            for (i = 12;  i < 24; i++) {
+                cout << seats[0][ROWS] << ' ';
             }
-            if (row == 24) {
-                for (row <= 37; row++;) {
-                    cout << "B ";
-                    if (row == 37) {
-                        for (row <= 49; row++;) {
-                            cout << "A ";
-                        }
+        }
+        cout << endl;
+      
+        cout << endl<< "                  1 1 1 1"<<endl<<"1 2 3 4 5 6 7 8 9 0 1 2 3"<<endl;
+
+        cout << endl;
+        if (i == 24) {
+            for (i = 24;  i < 37; i++) {
+                cout << seats[1][ROWS] << ' ';
+            }
+        }
+        cout << endl;
+        if (i == 37) {
+            for (i = 37; i <= 49; i++) {
+                cout << seats[2][ROWS] << ' ';
+            }
+        }
+        cout << endl;
+                {
+
+                    cout <<endl<< "Enter a seat or Q to quit: ";
+                    string input;
+                    cin >> input;
+                    if (input == "Q" || input == "q") {
+                        break;
                     }
                 }
+
             }
-        }
     }
-}
