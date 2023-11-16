d1852587a538ef61282d93873f38a97177328292
RAY OTTO <rjotto123@gmail.com>
2023-11-16 12:09:56 -0500

    Completed P11-1

--- P11/P11.cpp
+++ P11/P11.cpp
@@ -1,55 +1,112 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
+
 
 using namespace std;
 
 const int ROWS = 13;
+const int COLUMNS = 4;
+
 int findRowIndex(int thisRow);
 int findSeatIndex(char thisSeat);
-void displaySeatColumn(char seats[][ROWS], int column);
+int findSeatLetter(char letter);
+void displaySeats(char seats[][ROWS]);
+void markSeatAsOccupied(char seats[][ROWS], int rowIndex, int seatIndex);
 
 int main(void) {
-    char seats[4][ROWS] = { {'D','D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D'}, {'C' ,'C' ,'C' ,'C' ,'C' ,'C' ,'C' ,'C' ,'C' ,'C' ,'C' ,'C'},{'B','B','B','B','B','B','B','B','B','B','B','B'},{'A','A','A','A','A','A','A','A','A','A','A','A'} };
-    
+    char seats[COLUMNS][ROWS] = {
+
+        {'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D'},
+        {'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C'},
+        {'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B'},
+        {'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A'}
+    };
     while (true) {
-        int i = 0;
-        int j = 0;
+        displaySeats(seats);
 
-        for (i = 0; i < 12; i++) {
-            cout << seats[-1][ROWS]<< ' ';
-        }
-        cout << endl;
-        if (i == 12) {
-            for (i = 12;  i < 24; i++) {
-                cout << seats[0][ROWS] << ' ';
-            }
-        }
-        cout << endl;
-      
-        cout << endl<< "                  1 1 1 1"<<endl<<"1 2 3 4 5 6 7 8 9 0 1 2 3"<<endl;
 
+        cout << endl << "Enter a seat or Q to quit: ";
+        string input;
+        cin >> input;
         cout << endl;
-        if (i == 24) {
-            for (i = 24;  i < 37; i++) {
-                cout << seats[1][ROWS] << ' ';
-            }
-        }
-        cout << endl;
-        if (i == 37) {
-            for (i = 37; i <= 49; i++) {
-                cout << seats[2][ROWS] << ' ';
-            }
-        }
-        cout << endl;
-                {
 
-                    cout <<endl<< "Enter a seat or Q to quit: ";
-                    string input;
-                    cin >> input;
-                    if (input == "Q" || input == "q") {
-                        break;
-                    }
-                }
+        if (input == "Q" || input == "q") {
+            break;
+        }
 
-            }
+        string num = input.substr(0, input.length() - 1);
+        char letter = input[input.length() - 1];
+
+        int rowIndex = findRowIndex(stoi(num));
+        int seatIndex = findSeatIndex(letter);
+
+
+        if (rowIndex == -1 || seatIndex == -1){
+            cout << "Sorry, no such seat exists on the CRJ 200" << endl<<endl;
+        }
+        else if (input == "13D" || input=="13C") {
+            cout << "Sorry, no such seat exists on the CRJ 200" << endl << endl;
+        }
+        else {
+            markSeatAsOccupied(seats, rowIndex, seatIndex);
+        }
     }
+
+    return 0;
+}
+
+void displaySeats(char seats[][ROWS]) {
+
+    for (int i = 0; i < 2; ++i) {
+        for (int j = 0; j < ROWS; ++j) {
+            cout << seats[i][j] << ' ';
+
+        }
+        cout << endl;
+    }
+    cout << "\n" << "                  1 1 1 1" << endl << "1 2 3 4 5 6 7 8 9 0 1 2 3" << "\n" << endl;
+
+    for (int k = 2; k < 4; ++k) {
+        for (int l = 0; l < ROWS; ++l) {
+            cout << seats[k][l] << ' ';
+
+        }
+        cout << endl;
+    }
+
+}
+
+void markSeatAsOccupied(char seats[][ROWS], int rowIndex, int seatIndex) {
+
+
+    seats[seatIndex][rowIndex] = 'X';
+}
+
+int findRowIndex(int thisRow) {
+    if (thisRow > ROWS || thisRow < 1) {
+        return -1;
+    }
+    else {
+        return thisRow - 1; 
+    }
+}
+
+int findSeatLetter(char letter) {
+
+    if (letter == 'A') return 3;
+    if (letter == 'B') return 2;
+    if (letter == 'C') return 1;
+    if (letter == 'D') return 0;
+    return -1;
+}
+
+int findSeatIndex(char thisSeat) {
+   
+    if (thisSeat == 'A') return 3;
+
+    else if (thisSeat == 'B') return 2;
+    else if (thisSeat == 'C') return 1;
+    else if (thisSeat == 'D') return 0;
+    else return -1;
+}
